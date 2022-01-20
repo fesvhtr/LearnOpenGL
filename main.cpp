a0f54a3723111894eb8c5fd0127185a5c72672e1
fesvhtr <fesvhtr@gmail.com>
2022-01-20 21:06:55 +0800

    DEPTH TEST AND STENCIL TEST

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 //glm::mat4 transform();
 unsigned int loadTexture(const char *path);
-
+void drawLightCube(Shader lt_shader,unsigned int VAO);
 
 // global settings
 const unsigned int SCR_WIDTH = 800;
@@ -105,23 +105,32 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
-
     // tell GLFW to capture our mouse
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
     // glad: load all OpenGL function pointers
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+    //config global opengl test-----------------------------------
     //z-buffer
     glEnable(GL_DEPTH_TEST);
+    //set z-buffer type
+    glDepthFunc(GL_LESS);
+    //glDepthFunc(GL_ALWAYS);
+    //------------------------------------------------
+    //stencil buffer
+    glEnable(GL_STENCIL_TEST);
+    glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+    glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
     // build and compile our shader program
     Shader ourShader("D:/projects for lessons/CGchuyan/texture.vs",
                      "D:/projects for lessons/CGchuyan/texture.fs");
     Shader lt_shader("D:/projects for lessons/CGchuyan/lt_shader.vs",
                         "D:/projects for lessons/CGchuyan/lt_shader.fs");
+    Shader edge_shader("D:/projects for lessons/CGchuyan/texture.vs",
+                       "D:/projects for lessons/CGchuyan/shaderSingleColor.fs");
     //set obj
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -137,7 +146,6 @@ int main()
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
-
     //light attribute
     unsigned int lightVAO;
     glGenVertexArrays(1, &lightVAO);
@@ -148,9 +156,9 @@ int main()
     //set textures
 //    TEXTURE texture1("D:/projects for lessons/CGchuyan/zzface.jpg");
 //    TEXTURE texture2("D:/projects for lessons/CGchuyan/container.jpg");
-    unsigned int diffuse_map1 = loadTexture("D:/projects for lessons/CGchuyan/zzface.jpg");
+    unsigned int diffuse_map1 = loadTexture("D:/projects for lessons/CGchuyan/medo.jpg");
     unsigned int diffuse_map2 = loadTexture("D:/projects for lessons/CGchuyan/container.jpg");
-
+    //set shader int for each TEXTURE N
     ourShader.use();
     ourShader.setInt("material_zzface.diffuse",0);
     ourShader.setInt("material_container.diffuse",1);
@@ -164,50 +172,77 @@ int main()
         processInput(window);
         // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        //clear depth each render -else remain last z
+        //clear color buffer
+        //clear stencil buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+        //avoid depth buffer clear
+        //glDepthMask(GL_FALSE);
         // Active and bind Textures
 //        glActiveTexture(GL_TEXTURE0);
 //        glActiveTexture(GL_TEXTURE1);
-        // render the triangle
-        lt_shader.use();
+        //-----------------------------------------
+        //render light cube
+        glStencilMask(0x00);
+        drawLightCube(lt_shader,VAO);
+        //init all matrices
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
-        glm::mat4 lt_model = glm::mat4(1.0f);
-        glm::mat4 lt_trans = glm::mat4(1.0f);
+        //get view and projection which not change
         view = camera.GetViewMatrix();
-        lt_model = glm::translate(lt_model,lightPos);
         projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        lt_trans = projection * view * lt_model;
-        lt_shader.setMat4("lt_trans",lt_trans);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        glBindVertexArray(VAO);
+        // render the triangle
         for (unsigned int i = 0; i < 10; i++)
         {
             ourShader.use();
+            //set cube shader
             ourShader.setVec3("viewPos",camera.Position);
             ourShader.setVec3("light.position",lightPos);
             ourShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
             ourShader.setVec3("light.diffuse", 0.4f, 0.4f, 0.4f);
             ourShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+            //set textures
             ourShader.setVec3("material_zzface.specular", 0.5f, 0.5f, 0.5f);
             ourShader.setFloat("material_zzface.shininess", 64.0f);
             ourShader.setVec3("material_container.specular", 0.5f, 0.5f, 0.5f);
             ourShader.setFloat("material_container.shininess", 64.0f);
+            //1st. render pass, draw objects as normal, writing to the stencil buffer
+            glStencilFunc(GL_ALWAYS, 1, 0xFF);
+            glStencilMask(0xFF);
+            //texture active and bind
             glActiveTexture(GL_TEXTURE0);
             glBindTexture(GL_TEXTURE_2D, diffuse_map1);
             glActiveTexture(GL_TEXTURE1);
             glBindTexture(GL_TEXTURE_2D, diffuse_map2);
-            // init all matrices
+            // init model and trans each render
             glm::mat4 model= glm::mat4(1.0f);
             glm::mat4 trans = glm::mat4(1.0f);
+            //calculate model
             model = glm::translate(model, cubePositions[i]);
-            lt_model = glm::translate(lt_model,lightPos);
             float angle = 20.0f * (i+1);
-            model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
+            //model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
+            model = glm::rotate(model,  glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
             trans = projection * view * model;
             ourShader.setMat4("model",model);
             ourShader.setMat4("trans", trans);
             glDrawArrays(GL_TRIANGLES, 0, 36);
+            //2st
+            glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+            glStencilMask(0x00);
+            glDisable(GL_DEPTH_TEST);
+            edge_shader.use();
+            model= glm::mat4(1.0f);
+            trans = glm::mat4(1.0f);
+            //calculate model
+            model = glm::translate(model, cubePositions[i]);
+            //model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
+            model = glm::rotate(model,  glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
+            model = glm::scale(model,glm::vec3(1.1f,1.1f,1.1f));
+            trans = projection * view * model;
+            edge_shader.setMat4("model",model);
+            edge_shader.setMat4("trans", trans);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+            glStencilMask(0xFF);
         }
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
@@ -235,7 +270,6 @@ void processInput(GLFWwindow *window)
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
-
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -283,6 +317,7 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
+//load texture
 unsigned int loadTexture(char const * path)
 {
     unsigned int textureID;
@@ -318,4 +353,22 @@ unsigned int loadTexture(char const * path)
     }
 
     return textureID;
+}
+
+void drawLightCube(Shader lt_shader,unsigned int VAO){
+    //init all matrices
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
+    glm::mat4 lt_model = glm::mat4(1.0f);
+    glm::mat4 lt_trans = glm::mat4(1.0f);
+    //get view and projection which not change
+    view = camera.GetViewMatrix();
+    lt_model = glm::translate(lt_model,lightPos);
+    projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    lt_trans = projection * view * lt_model;
+    lt_shader.use();
+    lt_shader.setMat4("lt_trans",lt_trans);
+    //draw light cube
+    glDrawArrays(GL_TRIANGLES,0,36);
+    glBindVertexArray(VAO);
 }
